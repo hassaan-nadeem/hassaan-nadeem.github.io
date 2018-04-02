ca337449ba391d903cd55a3eb3878614e702d09b
Farzan Nadeem <mfnadeem@edu.uwaterloo.ca>
2018-04-01 22:21:42 -0400

    Fixed bug where URL was not relative

--- js/opening.js
+++ js/opening.js
@@ -2,10 +2,4 @@ $(document).ready(function(){
 	$('.slider').slider();
 });
 
-window.onload = function(){
-	document.getElementById("portal").addEventListener("click", NavigateToPortfolio);
-}
 
-function NavigateToPortfolio(){
-	window.location.replace("https://ece.uwaterloo.ca/~mfnadeem/hassaanportfolio/Portfolio.html");
-}
